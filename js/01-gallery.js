85bef3132922f1d427c28352ae207910e562b2c6
m-ivkin <nikolasivkin@gmail.com>
2023-01-24 11:03:26 +0000

    task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,46 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+function createGallery(imagesList) {
+  return imagesList
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>
+    `;
+    })
+    .join("");
+}
+
+const galleryList = createGallery(galleryItems);
+gallery.insertAdjacentHTML("afterbegin", galleryList);
+
+gallery.addEventListener("click", handleClick);
+
+function handleClick(e) {
+  if (e.target === e.currentTarget) {
+    return;
+  }
+  e.preventDefault();
+  const instance = basicLightbox.create(`
+    <img src="${e.target.dataset.source}" width="800" height="600">
+`);
+  instance.show();
+
+  window.addEventListener("keydown", escapeClose.bind(instance));
+}
+
+function escapeClose(e) {
+  if (e.code === "Escape") {
+    this.close();
+  }
+}
