d81cb90affe0d660202814237f739a7308caeec8
m-ivkin <nikolasivkin@gmail.com>
2023-01-25 06:27:55 +0000

    t2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,18 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+function createGallery(imagesList) {
+  return imagesList
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+    `;
+    })
+    .join('');
+}
+
+const galleryList = createGallery(galleryItems);
+gallery.insertAdjacentHTML('afterbegin', galleryList);
